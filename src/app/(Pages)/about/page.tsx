6e3d5a2747b5ebe81931fb0f4e4e63b6f3bd8aa0
Abhishek Gupta <yourabhishekgupta@gmail.com>
2024-12-25 12:16:34 +0530

    fixed some eslint error

--- src/app/(Pages)/about/page.tsx
+++ src/app/(Pages)/about/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
         </div>
         <div className="md:7/12 lg:w-6/12">
           <h2 className="text-2xl  font-bold md:text-4xl">About me</h2>
-          <p className="mt-6 text-gray-600">I'm Abhishek Gupta, a passionate full-stack MERN developer dedicated to creating innovative web and mobile solutions.</p>
+          <p className="mt-6 text-gray-600">I&apos;m Abhishek Gupta, a passionate full-stack MERN developer dedicated to creating innovative web and mobile solutions.</p>
           <p className="mt-4 text-gray-600"> With expertise in both frontend and backend technologies, I specialize in building scalable, user-friendly applications. Outside work, I enjoy sharing knowledge through my tech blog and YouTube channel.</p>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import { Copy } from "lucide-react"
+// import { Copy } from "lucide-react"
  
-import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+// import { Button } from "@/components/ui/button"
+// import {
+//   Dialog,
+//   DialogClose,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from "@/components/ui/dialog"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
 import HeroSection from '@/components/HeroSection'
 
 export default function Home() {
